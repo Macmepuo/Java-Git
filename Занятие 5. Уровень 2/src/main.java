43e4e65be16e609c84f60f3c8695982ce42233df
Macmepuo <wlad_sergeew2@mail.ru>
2020-12-14 16:56:23 +0300

    Занятие 5

---
+++ Занятие 5. Уровень 2/src/main.java
@@ -0,0 +1,9 @@
+public class main
+{
+    public static void main (String[] args)
+    {
+        Arr check = new Arr();
+        check.CheckTimeOfCycle();
+        check.CheckTimeOfCopy();
+    }
+}
